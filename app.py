77649f04cb94c18ce973c0e6c5437e100424f437
Lukealveshow <92194606+Lukealveshow@users.noreply.github.com>
2023-12-30 15:12:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,6 @@ from summarization import summarize_text
 from generation import generate_answer
 from database import insert_data
 
-db_secrets = st.secrets["connections.mysql"]
-
-db_username = db_secrets["username"]
-db_password = db_secrets["password"]
-
 def translate_page(language):
     # Using deep_translator for page translation
     translator = GoogleTranslator(source='auto', target=language)
@@ -63,8 +58,8 @@ def translate_page(language):
         answer = generate_answer(question, text_generation)
         translated_text = GoogleTranslator(source='auto', target=language).translate(text_translation)
 
-        # Utilize db_username e db_password conforme necessário
-        insert_data(db_username, db_password, name, age, gender, text_summarization, summarized_text, text_generation,
+        # Utilize a função insert_data do database.py
+        insert_data(name, age, gender, text_summarization, summarized_text, text_generation,
                     question, answer, text_translation, language, translated_text)
         st.success(translator.translate("Dados inseridos com sucesso!"))
 
