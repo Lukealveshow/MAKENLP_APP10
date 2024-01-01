10ffb3811437af673de2625ff0a4d5e07e809ddb
Lukealveshow <92194606+Lukealveshow@users.noreply.github.com>
2024-01-01 03:06:55 -0300

    Update database.py

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ db_secrets = st.secrets.get("connections.mysql", {})
 # Utilize os segredos conforme necessário
 db_username = db_secrets.get("username", "")
 db_password = db_secrets.get("password", "")
-db_database = db.secrets.get("database", "")
+db_database = db_secrets.get("database", "")
 db_config = {
     'host': 'localhost',
     'user': db_username,
