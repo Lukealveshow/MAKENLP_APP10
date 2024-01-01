3811885a370278ae2960de5d05cebdc6978b1da3
Lukealveshow <92194606+Lukealveshow@users.noreply.github.com>
2024-01-01 15:35:45 -0300

    Update database3.py

--- database3.py
+++ database3.py
@@ -21,11 +21,21 @@ def insert_data(name, age, gender, text_summarization, summarized_text, text_gen
         insert_query = '''INSERT INTO app_dados(name, age, gender, text_summarization, summarized_text,
           text_generation, question, answer, text_translation, language, translated_text)
           VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
+        
+        # Imprime os valores antes de executar a query
+        print("Valores a serem inseridos:", (name, age, gender, text_summarization, summarized_text, text_generation,
+                                      question, answer, text_translation, language, translated_text))
+        
         cursor.execute(insert_query, (name, age, gender, text_summarization, summarized_text, text_generation,
                                       question, answer, text_translation, language, translated_text))
+        
         conn.commit()
         cursor.close()
         conn.close()
+        
+        # Imprime uma mensagem de sucesso após o commit
+        print("Data committed successfully.")
+        
         st.success("Dados inseridos com sucesso!")
     except pymysql.Error as err:
         print("MySQL Error during insertion:", err)
