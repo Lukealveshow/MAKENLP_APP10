c6fbca6c39c8e78b7e6b6293e3e8284bb940fa15
Lukealveshow <92194606+Lukealveshow@users.noreply.github.com>
2024-01-02 10:52:50 -0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -26,55 +26,69 @@ def run_query(query):
         result = cursor.fetchall()
     connection.close()
     return result
-
+def insert_data(name, age, gender, text_summarization, summarized_text, text_generation, question,
+                answer, text_translation, language, translated_text):
+    try:
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+        insert_query = '''INSERT INTO app_dados(name, age, gender, text_summarization, summarized_text,
+          text_generation, question, answer, text_translation, language, translated_text)
+          VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
+        cursor.execute(insert_query, (name, age, gender, text_summarization, summarized_text, text_generation,
+                                      question, answer, text_translation, language, translated_text))
+        conn.commit()
+        cursor.close()
+        conn.close()
+    except mysql.connector.Error as err:
+        print("MySQL Error:", err)
 # Função para traduzir a página.
 def translate_page(language):
+    # Using deep_translator for page translation
     translator = GoogleTranslator(source='auto', target=language)
     translated_title = translator.translate("Seja Bem Vindo ao MAKENLP")
     st.title(translated_title)
-
-    name_key = f'name_{language}'
+    # Additional fields
     translated_name = translator.translate("Nome:")
-    name = st.text_input(translated_name, key=name_key)
-
-    age_key = f'age_{language}'
+    name = st.text_input(translated_name)
     translated_age = translator.translate("Idade:")
-    age = st.number_input(translated_age, step=1, key=age_key)
-
-    gender_key = f'gender_{language}'
+    age = st.number_input(translated_age, step=1)
     translated_gender = translator.translate("Gênero:")
     gender_options = [translator.translate('Masculino'), translator.translate('Feminino')]
-    gender = st.selectbox(translated_gender, options=gender_options, key=gender_key)
-
-    text_summarization_key = f'text_summarization_{language}'
+    gender = st.selectbox(translated_gender, options=gender_options)
+    # Section for text input
     translated_text_input = translator.translate("Digite o texto para resumir:")
-    text_summarization = st.text_area(translated_text_input, height=150, key=text_summarization_key)
-
-    text_generation_key = f'text_generation_{language}'
+    st.subheader(translated_text_input)
+    text_summarization = st.text_area("", height=150)
+    # Button to summarize text
+    if st.button(translator.translate("Resumir Texto")):
+        summarized_text = summarize_text(text_summarization)
+        st.subheader(translator.translate("Texto Resumido"))
+        st.write(summarized_text)
     translated_text_gen = translator.translate("Digite o texto:")
     st.subheader(translated_text_gen)
-    text_generation = st.text_area("", height=150, key=text_generation_key)
-
-    question_key = f'question_{language}'
+    text_generation = st.text_area("", height=150, key='text_generation')
     translated_question = translator.translate("Faça uma pergunta sobre o texto:")
-    question = st.text_input(translated_question, key=question_key)
+    question = st.text_input(translated_question, key='question')
 
-    text_translation_key = f'text_translation_{language}'
+    # Button to generate answer
+    if st.button(translator.translate("Gerar Resposta")):
+        answer = generate_answer(question, text_generation)
+        st.subheader(translator.translate("Resposta Gerada"))
+        st.write(answer)
+
+    # Section for text input and language selection
     translated_text_trans = translator.translate("Digite o texto para traduzir:")
     st.subheader(translated_text_trans)
-    text_translation = st.text_area("", height=150, key=text_translation_key)
-
-    language_key = f'language_{language}'
+    text_translation = st.text_area("", height=150, key='text_translation')
     translated_lang = translator.translate("Selecione o idioma de destino:")
     language_options = ['en', 'es', 'fr', 'pt']
-    selected_language = st.selectbox(translated_lang, options=language_options, key=language_key)
+    language = st.selectbox(translated_lang, options=language_options, key='language')
 
+    # Button to translate text
     if st.button(translator.translate("Traduzir Texto")):
-        translated_text = GoogleTranslator(source='auto', target=selected_language).translate(text_translation)
+        translated_text = GoogleTranslator(source='auto', target=language).translate(text_translation)
         st.subheader(translator.translate("Texto Traduzido"))
         st.write(translated_text)
-
-    # Restante do código...
 
     if st.button(translator.translate("Enviar")):
         summarized_text = summarize_text(text_summarization)
