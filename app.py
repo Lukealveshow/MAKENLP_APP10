16141f57d16a660e41daa3b1b9f3d3f8744fa6c7
Lukealveshow <92194606+Lukealveshow@users.noreply.github.com>
2024-01-03 16:29:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from deep_translator import GoogleTranslator
 from summarization import summarize_text
 from generation import generate_answer
 from database import insert_data
+
 def translate_page(language):
     # Using deep_translator for page translation
     translator = GoogleTranslator(source='auto', target=language)
@@ -10,23 +11,20 @@ def translate_page(language):
     st.title(translated_title)
     
     # Additional fields
-    name_key = f'name_{language}'
     translated_name = translator.translate("Nome:")
-    name = st.text_input(translated_name, key=name_key)
+    name = st.text_input(translated_name)
     
-    age_key = f'age_{language}'
     translated_age = translator.translate("Idade:")
-    age = st.number_input(translated_age, step=1, key=age_key)
+    age = st.number_input(translated_age, step=1)
     
-    gender_key = f'gender_{language}'
     translated_gender = translator.translate("Gênero:")
     gender_options = [translator.translate('Masculino'), translator.translate('Feminino')]
-    gender = st.selectbox(translated_gender, options=gender_options, key=gender_key)
+    gender = st.selectbox(translated_gender, options=gender_options)
     
     # Section for text input
-    text_summarization_key = f'text_summarization_{language}'
     translated_text_input = translator.translate("Digite o texto para resumir:")
-    text_summarization = st.text_area(translated_text_input, height=150, key=text_summarization_key)
+    st.subheader(translated_text_input)
+    text_summarization = st.text_area("", height=150)
     
     # Button to summarize text
     if st.button(translator.translate("Resumir Texto")):
@@ -34,37 +32,48 @@ def translate_page(language):
         st.subheader(translator.translate("Texto Resumido"))
         st.write(summarized_text)
     
-    text_generation_key = f'text_generation_{language}'
     translated_text_gen = translator.translate("Digite o texto:")
     st.subheader(translated_text_gen)
-    text_generation = st.text_area("", height=150, key=text_generation_key)
+    text_generation = st.text_area("", height=150, key='text_generation')
     
-    question_key = f'question_{language}'
     translated_question = translator.translate("Faça uma pergunta sobre o texto:")
-    question = st.text_input(translated_question, key=question_key)
-    
+    question = st.text_input(translated_question, key='question')
+
     # Button to generate answer
     if st.button(translator.translate("Gerar Resposta")):
         answer = generate_answer(question, text_generation)
         st.subheader(translator.translate("Resposta Gerada"))
         st.write(answer)
-    
+
     # Section for text input and language selection
-    text_translation_key = f'text_translation_{language}'
     translated_text_trans = translator.translate("Digite o texto para traduzir:")
     st.subheader(translated_text_trans)
-    text_translation = st.text_area("", height=150, key=text_translation_key)
+    text_translation = st.text_area("", height=150, key='text_translation')
     
-    language_key = f'language_{language}'
     translated_lang = translator.translate("Selecione o idioma de destino:")
     language_options = ['en', 'es', 'fr', 'pt']
-    selected_language = st.selectbox(translated_lang, options=language_options, key=language_key)
-    
+    language = st.selectbox(translated_lang, options=language_options, key='language')
+
     # Button to translate text
     if st.button(translator.translate("Traduzir Texto")):
-        translated_text = GoogleTranslator(source='auto', target=selected_language).translate(text_translation)
+        translated_text = GoogleTranslator(source='auto', target=language).translate(text_translation)
         st.subheader(translator.translate("Texto Traduzido"))
         st.write(translated_text)
+
+    # Button to execute all functions and insert into the database
+    if st.button(translator.translate("Enviar")):
+        summarized_text = summarize_text(text_summarization)
+        answer = generate_answer(question, text_generation)
+        translated_text = GoogleTranslator(source='auto', target=language).translate(text_translation)
+
+        # Utilize a função insert_data_mongodb do database.py
+        print(name, age, gender, text_summarization, summarized_text, text_generation,
+        question, answer, text_translation, language, translated_text)
+
+        # Chame a função insert_data depois desta linha
+        insert_data(name, age, gender, text_summarization, summarized_text, text_generation,
+                    question, answer, text_translation, language, translated_text)
+
 
 # Run the Streamlit app
 if __name__ == '__main__':
