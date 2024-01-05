b8fda01b3e8743c016b3cb811414950b8dd68670
Lukealveshow <92194606+Lukealveshow@users.noreply.github.com>
2024-01-04 23:46:48 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 import toml
-import pymysql
+import mysql.connector
 import streamlit as st
 
 credentials = toml.load("secrets.toml")["mysql"]
@@ -8,20 +8,21 @@ db_config = {
     'host': 'sql3.freesqldatabase.com',
     'port': 3306,
     'database': 'sql3674795',
-    'username': 'sql3674795',
+    'user': 'sql3674795',
     'password': 'FekLG4Q6tE'
 }
 
 def insert_data(name, age, gender, text_summarization, summarized_text, text_generation, question,
                 answer, text_translation, language, translated_text):
     try:
-        conn = pymysql.connect(**db_config)
+        conn = mysql.connector.connect(**db_config)
         cursor = conn.cursor()
         insert_query = '''INSERT INTO app_dados(name, age, gender, text_summarization, summarized_text,
           text_generation, question, answer, text_translation, language, translated_text)
           VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
-        cursor.execute(insert_query, (name, age, gender, text_summarization, summarized_text, text_generation,
-                                      question, answer, text_translation, language, translated_text))
+        values = (name, age, gender, text_summarization, summarized_text, text_generation,
+                  question, answer, text_translation, language, translated_text)
+        cursor.execute(insert_query, values)
         conn.commit()
         cursor.close()
         conn.close()
