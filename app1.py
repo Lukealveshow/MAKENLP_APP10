404b3ae5054d0b14b2c3423e05c74b969309235e
Lukealveshow <92194606+Lukealveshow@users.noreply.github.com>
2024-01-05 21:29:00 -0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -2,7 +2,7 @@ import streamlit as st
 from deep_translator import GoogleTranslator
 from summarization import summarize_text
 from generation import generate_answer
-from database import insert_data
+from database3 import insert_data
 import _thread
 import weakref
 
